d86e2b8953b5536fbc883fe75f6439a0e5b05522
rafmicheal <ifeanyiwebactivity@gmail.com>
2025-09-01 01:17:52 +0100

    did the CRUD THING

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # add this line:
+    'bookshelf.apps.BookshelfConfig',
 ]
 
 MIDDLEWARE = [
