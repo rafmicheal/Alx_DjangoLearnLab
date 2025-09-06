f69c33decc8d387c4a10618eff5812669e3175bc
rafmicheal <ifeanyiwebactivity@gmail.com>
2025-09-06 10:49:54 +0100

    done

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,8 +1,16 @@
-from relationship_app.models import Library, Book
+from bookshelf.models import Book
+from relationship_app.models import Author, Library
 
-# Example variable
-library_name = "Central Library"
+# Example: Get all books
+all_books = Book.objects.all()
+print(all_books)
 
-# Retrieve a library by its name
-library = Library.objects.get(name=library_name)
-print(library)
+# Example: Get all books by a specific author
+author = Author.objects.get(id=1)
+books_by_author = author.books.all()
+print(books_by_author)
+
+# Example: Get all books in a specific library
+library = Library.objects.get(id=1)
+books_in_library = library.books.all()
+print(books_in_library)
