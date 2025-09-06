77ee6b2651e43be1c6eff1c3ae92e060eae4412f
rafmicheal <ifeanyiwebactivity@gmail.com>
2025-09-06 11:59:39 +0100

    done

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
-from .views import list_books, LibraryDetailView
+# django_models/urls.py
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),  # Function-based view
-    path('library/<int:pk>/', LibraryDetailView.as_view(),
-         name='library_detail'),  # Class-based view
+    path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')),  # include app URLs
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,19 +1,12 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
-from .models import Book, Library
-
-# Function-based view to list all books
-
-
+# Function-based view
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-
-# Class-based view to display details for a specific library
+# Class-based view
 
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
