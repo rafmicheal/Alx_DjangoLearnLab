62d13aca6634ca7fa3fb48c2c6cfa8953ea9b626
rafmicheal <ifeanyiwebactivity@gmail.com>
2025-09-07 22:10:33 +0100

    done

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('member-view/', views.member_view, name='member_view'),
 
     # Task 4: Permission-Based Views (prep)
-    path('book/add/', views.add_book, name='add_book'),
-    path('book/<int:pk>/edit/', views.edit_book, name='edit_book'),
-    path('book/<int:pk>/delete/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 ]
