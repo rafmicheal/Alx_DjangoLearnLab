a9131c1811726dcf8dfc29e3573c492d43e6c196
rafmicheal <ifeanyiwebactivity@gmail.com>
2025-09-07 13:58:58 +0100

    done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, redirect
 from django.views.generic import DetailView
 from django.contrib.auth import login, authenticate, logout
@@ -65,3 +67,34 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return render(request, 'relationship_app/logout.html')
+
+
+# --- Role checks ---
+
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+
+# --- Role-based views ---
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
