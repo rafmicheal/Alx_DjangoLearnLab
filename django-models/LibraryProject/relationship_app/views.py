161efa50cbd0306e3bc0f3474eea7cdd5f068601
rafmicheal <ifeanyiwebactivity@gmail.com>
2025-09-07 22:20:06 +0100

    done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -41,17 +41,17 @@ def is_member(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
 
-@user_passes_test(is_admin)
+@user_passes_test(is_admin, login_url='/login')
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
 
-@user_passes_test(is_librarian)
+@user_passes_test(is_librarian, login_url='/login')
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
 
-@user_passes_test(is_member)
+@user_passes_test(is_member, login_url='/login')
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
 
