fc48df9b26d50d997cbec6d11ed5e0c41993c042
rafmicheal <ifeanyiwebactivity@gmail.com>
2025-09-09 11:18:41 +0100

    Fix Task 3 role-based access views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -31,28 +31,28 @@ class LibraryDetailView(DetailView):
 
 
 def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+    return user.userprofile.role == 'Admin'
 
 
 def is_librarian(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+    return user.userprofile.role == 'Librarian'
 
 
 def is_member(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+    return user.userprofile.role == 'Member'
 
 
-@user_passes_test(is_admin, login_url='login')
+@user_passes_test(is_admin, login_url='/login/')
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
 
-@user_passes_test(is_librarian, login_url='login')
+@user_passes_test(is_librarian, login_url='/login/')
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
 
-@user_passes_test(is_member, login_url='login')
+@user_passes_test(is_member, login_url='/login/')
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
 
