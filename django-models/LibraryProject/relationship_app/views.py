aa16e69f5f674a8d50ee79e80f8f8d2872b1f4ec
rafmicheal <ifeanyiwebactivity@gmail.com>
2025-09-09 11:12:11 +0100

    Fix role-based views @user_passes_test login_url

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -42,17 +42,17 @@ def is_member(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
 
-@user_passes_test(is_admin, login_url='/login/')
+@user_passes_test(is_admin, login_url='login')
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
 
-@user_passes_test(is_librarian, login_url='/login/')
+@user_passes_test(is_librarian, login_url='login')
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
 
-@user_passes_test(is_member, login_url='/login/')
+@user_passes_test(is_member, login_url='login')
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
 
