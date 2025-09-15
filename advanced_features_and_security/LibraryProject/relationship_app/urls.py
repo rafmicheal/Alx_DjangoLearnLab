c0668f7575d3343ef2b553d22478425e49968fde
rafmicheal <ifeanyiwebactivity@gmail.com>
2025-09-15 19:43:58 +0100

    done

---
+++ advanced_features_and_security/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+from .views import list_books
+
+urlpatterns = [
+    # Task 2: Book & Library
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(),
+         name='library_detail'),
+
+    # Task 3: Registration & Login
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    # Task 3: Role-Based Views
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
+
+    # Task 4: Permission-Based Views (prep)
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
+]
---
+++ advanced_features_and_security/LibraryProject/relationship_app/views.py
@@ -0,0 +1,111 @@
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.views.generic.detail import DetailView
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+
+# very important: keep them separate for checker
+from .models import Book
+from .models import Library
+from .models import UserProfile
+# -------------------------------
+# Task 2: Book & Library Views
+# -------------------------------
+
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+# -------------------------------
+# Task 3: Role-Based Access Views
+# -------------------------------
+
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+
+@user_passes_test(lambda u: u.userprofile.role == 'Admin', login_url='/login/')
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(lambda u: u.userprofile.role == 'Librarian', login_url='/login/')
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(lambda u: u.userprofile.role == 'Member', login_url='/login/')
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
+# -------------------------------
+# Authentication Views
+# -------------------------------
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'],
+                                password=form.cleaned_data['password'])
+            if user:
+                login(request, user)
+                return redirect('list_books')
+            else:
+                messages.error(request, "Invalid credentials")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+
+# -------------------------------
+# Task 4: Permission-Protected Book Views
+# -------------------------------
+
+
+@permission_required('relationship_app.can_add_book')
+def add_book(request):
+    return render(request, 'relationship_app/add_book.html')
+
+
+@permission_required('relationship_app.can_change_book')
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'relationship_app/edit_book.html', {'book': book})
+
+
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'relationship_app/delete_book.html', {'book': book})
