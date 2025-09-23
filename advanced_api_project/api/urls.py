aa1029c65417029617d3a1815c6a119011a8d98a
rafmicheal <ifeanyiwebactivity@gmail.com>
2025-09-23 11:16:36 +0100

    remove it

--- advanced_api_project/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('authors/', views.AuthorList.as_view(), name='author-list'),
-    path('books/', views.BookList.as_view(), name='book-list'),
-]
