9a34b2a3e47b2a395a110d602844625cf3d7d3a2
rafmicheal <ifeanyiwebactivity@gmail.com>
2025-10-05 06:03:43 +0100

    done

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,9 +18,9 @@ urlpatterns = [
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 
     # Comment CBVs
-    path('posts/<int:post_id>/comments/new/',
+    path('posts/<int:pk>/comments/new/',
          views.CommentCreateView.as_view(), name='comment-create'),
-    path('comments/<int:pk>/edit/',
+    path('comments/<int:pk>/update/',
          views.CommentUpdateView.as_view(), name='comment-update'),
     path('comments/<int:pk>/delete/',
          views.CommentDeleteView.as_view(), name='comment-delete'),
