e88af7c5b48441388e858590414b0029674a0ecc
rafmicheal <ifeanyiwebactivity@gmail.com>
2025-10-05 05:33:58 +0100

    done

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from .models import Post
 
